a88e77f1f4c03ba14f4863fa4dbf40ad1fd83773
DJCrespo <dj.crespo.castilla@gmail.com>
2022-03-24 18:28:17 -0600

    Add database in mongoDB

---
+++ test/test-database.js
@@ -0,0 +1,25 @@
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://dev:z2IpdJrKWLGb2miJ@conecta4.v4ocs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+      await client.connect();
+      const database = client.db('sample_mflix');
+      const movies = database.collection('movies');
+      // Query for a movie that has the title 'Back to the Future'
+      const query = { title: 'Back to the Future' };
+      const movie = await movies.findOne(query);
+      console.log(movie);
+    } catch (error) {
+        console.log(error)
+    }
+    finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+
+run()
